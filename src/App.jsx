329a57f38ea819560feb6b729f3a0d444add0620
NigelGroen5 <nigel.groen5@gmail.com>
2025-03-24 23:17:30 -0400

    More updates before pulling

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box} from "@radix-ui/themes";
+import { Box } from "@radix-ui/themes";
 import "./App.css";
+
 import Navbar from "./components/Navbar.jsx";
 import Meeting from "./pages/meeting.jsx";
 import Interview from "./pages/interview.jsx";
 import MeetingForm from "./components/MeetingForm.jsx";
 import MeetingSummary from "./components/MeetingSummary.jsx";
+import LoginPage from "./pages/LoginPage.jsx"; // 🆕 import LoginPage
 
 function App() {
   return (
@@ -16,12 +18,13 @@ function App() {
       <Router>
         <div className="bg-gray-100 h-full">
           <Navbar /> {/* Navbar is always visible */}
-            <Routes>
-              <Route path="/meeting" element={<Meeting />} />
-              <Route path="/interview" element={<Interview />} />
-              <Route path="/meeting-form/:date" element={<MeetingForm/>} />
-              <Route path="/meeting-summary" element={<MeetingSummary/>} />
-            </Routes>
+          <Routes>
+            <Route path="/meeting" element={<Meeting />} />
+            <Route path="/interview" element={<Interview />} />
+            <Route path="/meeting-form/:date" element={<MeetingForm />} />
+            <Route path="/meeting-summary" element={<MeetingSummary />} />
+            <Route path="/login" element={<LoginPage />} /> {/* 🆕 login route */}
+          </Routes>
         </div>
       </Router>
     </Box>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/LoginPage.jsx
+import React from 'react';
+import Login from '../components/Login';
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
